1df64115d1227467f41883df4fc6fc5411b07855
Marius Vestol <marius.vestol@gmail.com>
2024-10-24 03:16:27 +0800

    sjalla

--- armtest.py
+++ armtest.py
@@ -42,37 +42,42 @@ time.sleep(2)
 
 """
 
-grader_test=44
+grader_test=30
 
 
 time.sleep(1)
 
-for i in range(grader_test):
-	a-=1
-	b = deg(math.acos(math.sin(degtorad(a))-10/8.5)-degtorad(a))
-	c = 180-(a+b)
-	Arm.Arm_serial_servo_write(2, a, 500)
-	time.sleep(0.01)
-	Arm.Arm_serial_servo_write(3, b, 500)
-	time.sleep(0.01)
-	Arm.Arm_serial_servo_write(4, c, 500)
-	time.sleep(0.01)
-	
-print(a)
 
-time.sleep(1)
+for y in range(5):
+	grader_test-=1
 
-for i in range(grader_test):
-	a+=1
-	b = deg(math.acos(math.sin(degtorad(a))-10/8.5)-degtorad(a))
-	c = 180-(a+b)
-	Arm.Arm_serial_servo_write(2, a, 500)
-	time.sleep(0.01)
-	Arm.Arm_serial_servo_write(3, b, 500)
-	time.sleep(0.01)
-	Arm.Arm_serial_servo_write(4, c, 500)
-	time.sleep(0.01)
+	for i in range(grader_test):
+		a-=1
+		b = deg(math.acos(math.sin(degtorad(a))-10/8.5)-degtorad(a))
+		c = 180-(a+b)
+		Arm.Arm_serial_servo_write(2, a, 500)
+		time.sleep(0.001)
+		Arm.Arm_serial_servo_write(3, b, 500)
+		time.sleep(0.001)
+		Arm.Arm_serial_servo_write(4, c, 500)
+		time.sleep(0.001)
+		
+	print(a)
 
+	time.sleep(1)
+
+	for i in range(grader_test):
+		a+=1
+		b = deg(math.acos(math.sin(degtorad(a))-10/8.5)-degtorad(a))
+		c = 180-(a+b)
+		Arm.Arm_serial_servo_write(2, a, 500)
+		time.sleep(0.001)
+		Arm.Arm_serial_servo_write(3, b, 500)
+		time.sleep(0.001)
+		Arm.Arm_serial_servo_write(4, c, 500)
+		time.sleep(0.001)
+
+	time.sleep(4)
 
 
 	
