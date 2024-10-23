33395399ead3912b5ca0aa847b38a6e5f5d95ea4
Marius Vestol <marius.vestol@gmail.com>
2024-10-23 23:15:52 +0800

    hei

--- armtest.py
+++ armtest.py
@@ -1,3 +1,14 @@
-import Arm_Lib
+#!/usr/bin/env python3
+#coding=utf-8
+import time
+from Arm_Lib import Arm_Device
+#Create a robotic arm object
+Arm = Arm_Device()
+time.sleep(.1)
 
-#http://www.yahboom.net/study/Dofbot-Pi
+
+for i in range(90):
+	Arm.Arm_serial_servo_write(4, 180-i, 500)
+	time.sleep(.01)
+	Arm.Arm_serial_servo_write(3, 0+i, 500)
+	time.sleep(.01)
