231095aabbe45f0b5ab3702896e76accc017f779
Marius Vestol <marius.vestol@gmail.com>
2024-12-04 01:20:41 +0800

    ha

--- reload.py
+++ reload.py
@@ -1,1 +1,14 @@
+import time
+from Arm_Lib import Arm_Device
+import math
 
+
+Arm = Arm_Device()
+time.sleep(.1)
+
+
+def startposition():
+	pass
+
+Arm.Arm_serial_servo_write(2, 100, 500)
+time.sleep(0.001)
