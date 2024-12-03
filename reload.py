a659de3426ec46c1af51c06f782365faeed8cede
Marius Vestol <marius.vestol@gmail.com>
2024-12-04 02:01:12 +0800

    GATTA

--- reload.py
+++ reload.py
@@ -6,9 +6,49 @@ import math
 Arm = Arm_Device()
 time.sleep(.1)
 
+#Arm.Arm_serial_servo_write(6, 0, 500)
+time.sleep(.1)
 
 def startposition():
-	pass
+	Arm.Arm_serial_servo_write(1, 90, 500)
+	time.sleep(.1)
+	Arm.Arm_serial_servo_write(2, 135, 500)
+	time.sleep(.1)
+	Arm.Arm_serial_servo_write(3, -10, 500)
+	time.sleep(.1)
+	Arm.Arm_serial_servo_write(4, 45, 500)
+	time.sleep(1)
 
-Arm.Arm_serial_servo_write(2, 100, 500)
-time.sleep(0.001)
+
+
+startposition()
+
+def neger(pos1, pos2):
+	Arm.Arm_serial_servo_write(1, pos1, 500)
+	time.sleep(.1)
+	Arm.Arm_serial_servo_write(2, 78, 500)
+	time.sleep(.1)
+	Arm.Arm_serial_servo_write(3, 0, 500)
+	time.sleep(.1)	
+	Arm.Arm_serial_servo_write(4, 7, 500)
+	time.sleep(1)
+	Arm.Arm_serial_servo_write(6, pos2, 500)
+	time.sleep(1)
+
+#120 55 
+
+
+
+startposition()
+neger(195, 0)
+startposition()
+neger(195, 170)
+neger(-20, 0)
+startposition()
+neger(-20, 170)
+neger(120, 0)
+startposition()
+neger(120, 170)
+neger(55, 170)
+startposition()
+
